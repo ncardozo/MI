25ffb9c9f4067ef08ce3392d32e5a393db0bb7dc
Nicolas Cardozo <n.cardozo@uniandes.edu.co>
2018-06-09 19:58:58 -0500

    coding files

--- app/src/main/java/ncardozo/mi/Intruder.kt
+++ app/src/main/java/ncardozo/mi/Intruder.kt
@@ -1,4 +1,40 @@
 package ncardozo.mi
 
-class Intruder {
+import android.content.res.Resources
+import android.graphics.Bitmap
+import android.graphics.Canvas
+
+class Intruder(var img : Bitmap) {
+    var x = 0
+    var y = 0
+    var w = 0
+    var h = 0
+    private var xVelocity = 20
+    private var yVelocity = 20
+    private var screenWidth = Resources.getSystem().displayMetrics.widthPixels
+    private var screenHeight = Resources.getSystem().displayMetrics.heightPixels
+
+    init {
+        w = img.width
+        h = img.height
+        x = screenWidth/2
+        y = screenHeight/2
+    }
+
+    fun draw(canvas : Canvas) {
+        canvas.drawBitmap(img, x.toFloat(), y.toFloat(), null)
+    }
+
+    fun update() {
+        if(x > screenWidth - img.width || x < img.width) {
+            xVelocity = xVelocity * -1
+        }
+        if(y > screenHeight - img.height || y < img.height) {
+            yVelocity = yVelocity * -1
+        }
+
+        x += xVelocity
+        y += yVelocity
+    }
+
 }
--- app/src/main/java/ncardozo/mi/Surface.kt
+++ app/src/main/java/ncardozo/mi/Surface.kt
@@ -1,2 +1,53 @@
 package ncardozo.mi
 
+import android.content.*
+import android.graphics.BitmapFactory
+import android.graphics.Canvas
+import android.util.AttributeSet
+import android.view.SurfaceHolder
+import android.view.SurfaceView
+
+class Surface(context: Context, attributes:AttributeSet) : SurfaceView(context, attributes), SurfaceHolder.Callback {
+
+    private val thread : GameThread
+    private var intruder : Intruder? = null
+
+    init {
+        holder.addCallback(this)
+        thread = GameThread(holder, this)
+    }
+
+    override fun surfaceCreated(p0: SurfaceHolder?) {
+        intruder = Intruder(BitmapFactory.decodeResource(resources, R.drawable.green))
+
+        thread.setRunning(true)
+        thread.start()
+    }
+
+    override fun surfaceDestroyed(p0 : SurfaceHolder?) {
+        var retry = true
+        while(retry) {
+            try {
+                thread.setRunning(false)
+                thread.join()
+            } catch(e : Exception) {
+                e.printStackTrace()
+            }
+            retry = false
+        }
+    }
+
+    override fun surfaceChanged(p0: SurfaceHolder?, p1: Int, p2: Int, p3: Int) {
+
+    }
+
+    fun update() {
+        intruder!!.update()
+    }
+
+    override fun draw(canvas : Canvas) {
+        super.draw(canvas)
+
+        intruder!!.draw(canvas)
+    }
+}
