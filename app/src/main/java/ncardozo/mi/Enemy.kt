64800a5abb0057f46dda62a8da6cc6574b795eaa
Nicolas Cardozo <n.cardozo@uniandes.edu.co>
2018-06-10 10:09:00 -0500

    project

--- app/src/main/java/ncardozo/mi/Enemy.kt
+++ app/src/main/java/ncardozo/mi/Enemy.kt
@@ -4,21 +4,20 @@ import android.content.res.Resources
 import android.graphics.Bitmap
 import android.graphics.Canvas
 
-class Intruder(var img : Bitmap) {
+class Enemy(private val img : Bitmap ) {
     var x = 0
     var y = 0
     var w = 0
     var h = 0
-    private var xVelocity = 20
-    private var yVelocity = 20
+    private var xVelocity = 10
     private var screenWidth = Resources.getSystem().displayMetrics.widthPixels
     private var screenHeight = Resources.getSystem().displayMetrics.heightPixels
 
     init {
         w = img.width
         h = img.height
-        x = screenWidth/2
-        y = screenHeight/2
+        x = screenHeight - img.width
+        y = screenWidth - (img.height * 2)
     }
 
     fun draw(canvas : Canvas) {
@@ -26,15 +25,8 @@ class Intruder(var img : Bitmap) {
     }
 
     fun update() {
-        if(x > screenWidth - img.width || x < img.width) {
-            xVelocity = xVelocity * -1
-        }
-        if(y > screenHeight - img.height || y < img.height) {
-            yVelocity = yVelocity * -1
-        }
-
-        x += (xVelocity)
-        y += (yVelocity)
+        x -= (xVelocity)
+        //y = touch_y - h / 2
     }
 
 }
--- app/src/main/java/ncardozo/mi/Surface.kt
+++ app/src/main/java/ncardozo/mi/Surface.kt
@@ -6,11 +6,17 @@ import android.graphics.Canvas
 import android.util.AttributeSet
 import android.view.SurfaceHolder
 import android.view.SurfaceView
+import android.view.MotionEvent
 
 class Surface(context: Context, attributes:AttributeSet) : SurfaceView(context, attributes), SurfaceHolder.Callback {
 
     private val thread : GameThread
     private var intruder : Intruder? = null
+
+    private var enemy : Enemy? = null
+    private var touched = false
+    private var touched_x = 0
+    private var touched_y = 0
 
     init {
         holder.addCallback(this)
@@ -19,6 +25,7 @@ class Surface(context: Context, attributes:AttributeSet) : SurfaceView(context, 
 
     override fun surfaceCreated(p0: SurfaceHolder?) {
         intruder = Intruder(BitmapFactory.decodeResource(resources, R.drawable.green))
+        enemy = Enemy(BitmapFactory.decodeResource(resources, R.drawable.cop_car))
 
         thread.setRunning(true)
         thread.start()
@@ -43,11 +50,29 @@ class Surface(context: Context, attributes:AttributeSet) : SurfaceView(context, 
 
     fun update() {
         intruder!!.update()
+        //if(touched)
+            enemy!!.update()
     }
 
     override fun draw(canvas : Canvas) {
         super.draw(canvas)
 
         intruder!!.draw(canvas)
+        enemy!!.draw(canvas)
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        touched_x = event.x.toInt()
+        touched_y = event.y.toInt()
+
+        val action = event.action
+        when(action) {
+            MotionEvent.ACTION_DOWN -> touched = true
+            MotionEvent.ACTION_MOVE -> touched = true
+            MotionEvent.ACTION_UP -> touched = false
+            MotionEvent.ACTION_CANCEL ->  touched = false
+            MotionEvent.ACTION_OUTSIDE -> touched = false
+        }
+        return true
     }
 }
